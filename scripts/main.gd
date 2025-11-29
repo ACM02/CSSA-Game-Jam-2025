41cf362a118f6b9c4bd148b2dd74d76e475bd942
pieberrykinnie <dmtruong1980@gmail.com>
2025-11-29 16:19:04 -0600

    feat: Add intro text to the game

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,7 +4,11 @@ signal game_over
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# Play intro sequence
+	$HUD.play_narrative_sequence("One must imagine Sisyphus happy.")
+
+	# Wait for the HUD to signal that the text is finished
+	await $HUD.transition_finished
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
