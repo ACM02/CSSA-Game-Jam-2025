f9c4cf22b04225a43b95fada5ee22de55a005fa7
ACM02 <aidancmcleod@gmail.com>
2025-11-29 17:29:29 -0600

    Remove unused boulder code

--- scripts/boulder.gd
+++ scripts/boulder.gd
@@ -13,39 +13,3 @@ func try_push(vector):
 func _physics_process(delta: float) -> void:
 	var physics_effects = get_physics_effects()
 	move_and_collide(physics_effects * delta)
-	#velocity = physics_effects
-
-	# ----------------------------------------------------------
-	# 1. Check predictive collision ONCE
-	# ----------------------------------------------------------
-	#var predicted = move_and_collide(velocity * delta, true)
-#
-	#if predicted:
-		#var collider = predicted.get_collider()
-#
-		#if collider.is_in_group("player"):
-			#touching_player = true
-		#else:
-			#touching_player = false
-	#else:
-		#touching_player = false
-
-	# ----------------------------------------------------------
-	# 2. Apply movement logic based on touching status
-	# ----------------------------------------------------------
-	#if touching_player:
-		## Smooth movement: follow player's input direction continuously
-		#var pv = Vector2(
-			#Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-			#Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-		#)
-#
-		#if pv.length() > 0.1:
-			#velocity = pv.normalized() * speed
-		#else:
-			#velocity = Vector2.ZERO
-
-	# ----------------------------------------------------------
-	# 3. Move once
-	# ----------------------------------------------------------
-	#move_and_collide(velocity * delta)
