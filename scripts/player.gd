4e37f8624cc53e735df18aafb33613b01a171ea3
pieberrykinnie <dmtruong1980@gmail.com>
2025-11-29 19:20:36 -0600

    feat: Pause stamina recovery when touching boulder

--- scripts/player.gd
+++ scripts/player.gd
@@ -47,15 +47,21 @@ func _physics_process(delta):
 		
 	# Stamina regeneration
 	# Recovers when not pushing a boulder
-	if not is_pushing and stamina < max_stamina:
-		stamina += stamina_regen_rate * delta
-		if stamina > max_stamina:
-			stamina = max_stamina
-		stamina_change.emit(stamina)
+	if is_pushing:
+		# Stamina draining is handled in try_move upon successful push
+		pass
+	else:
+		# Check if touching boulder to prevent "cheese" (waiting against the rock)
+		if is_touching_boulder():
+			pass # Prevent regeneration
+		elif stamina < max_stamina:
+			stamina += stamina_regen_rate * delta
+			if stamina > max_stamina:
+				stamina = max_stamina
+			stamina_change.emit(stamina)
 
 func try_move(motion: Vector2, delta: float):
 	#print("Trying to move: " + str(motion))
-	var space = get_world_2d().direct_space_state
 	var collision = move_and_collide(motion)
 
 	if collision:
@@ -83,3 +89,24 @@ func try_move(motion: Vector2, delta: float):
 	# If no collision -> move normally
 	#print("Moving normally by: " + str(motion))
 	translate(motion)
+
+
+# Utility to check for contact even when stationary
+func is_touching_boulder() -> bool:
+	var shape_node = $CollisionShape2D
+	if not shape_node or not shape_node.shape:
+		return false
+
+	var params = PhysicsShapeQueryParameters2D.new()
+	params.shape = shape_node.shape
+	params.transform = shape_node.global_transform
+	params.collision_mask = collision_mask
+	params.margin = 2.0 # Detect objects within 2 pixels
+	params.exclude = [self.get_rid()] # Don't detect self
+	
+	var results = get_world_2d().direct_space_state.intersect_shape(params)
+	
+	for result in results:
+		if result.collider is Boudler:
+			return true
+	return false
